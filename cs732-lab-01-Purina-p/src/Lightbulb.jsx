d743f15a4558c8b139a10f2e663452aabb7d97e4
Purina-p <127057177+Purina-p@users.noreply.github.com>
2023-03-13 13:16:07 +1300

    Update Lightbulb.jsx

--- cs732-lab-01-Purina-p/src/Lightbulb.jsx
+++ cs732-lab-01-Purina-p/src/Lightbulb.jsx
@@ -5,9 +5,12 @@ function Lightbulb() {
 
     return (
         <button onClick={() => setisOn(!isOn)}>
-            {isOn ? <img src=".\public\light-off.png" alt="light-of" /> : <img src=".\public\light-on.png" alt="light-on" />}
+            {isOn ? <img src=".\public\light-off.png" alt="light-off" /> : <img src=".\public\light-on.png" alt="light-on" />}
         </button>
     )
 }
+
+// 设置isOn的初始值为true(默认他是关闭的),然后使用set方法来更新他的状态
+// 第二行看他的状态是什么，在他的前后进行改变
 
 export default Lightbulb;
