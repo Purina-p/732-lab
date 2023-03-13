bd5db4809d8d0ec30b4bbd9503734baaf3589c88
Purina-p <127057177+Purina-p@users.noreply.github.com>
2023-03-13 13:41:12 +1300

    exercise-8 success

--- cs732-lab-01-Purina-p/src/App.jsx
+++ cs732-lab-01-Purina-p/src/App.jsx
@@ -4,13 +4,33 @@ import './App.css';
 import Lightbulb from "./Lightbulb";
 import Todos from "./Todos"
 
+const intialtodos = [
+  { description: 'Finish lecture', isComplete: true },
+  { description: 'Do homework', isComplete: false },
+  { description: 'Sleep', isComplete: true }
+]
+
 function App() {
 
-  const todos=[
-    {description:'Finish lecture',isComplete:true},
-    {description:'Do homework',isComplete:false},
-    {description:'Sleep',isComplete:true}
-  ]
+  const[todos,setTodos]=useState(intialtodos);
+  // 将todos初始化，设为initialtodos
+
+  function ChangeIsComplete(todo,e){
+    
+    const index = todos.indexOf(todo);
+    // 寻找todo里面的索引
+    const newTodos = [...todos];
+    // 继承我之间改变过的或者是未改变的列表
+    newTodos[index] = {
+      description:todo.description,
+      isComplete:e
+    }
+    // 把有着特定index的todo改变他的description
+
+    setTodos([...newTodos])
+
+  }
+
 
   return (
     <div>
@@ -20,7 +40,7 @@ function App() {
       <h1>My light bulb</h1>
       <Lightbulb />
       <h1>My todos</h1>
-      <Todos todos={todos}/>
+      <Todos todos={todos} ChangeIsComplete={ChangeIsComplete}/>
 
     </div>
   );
