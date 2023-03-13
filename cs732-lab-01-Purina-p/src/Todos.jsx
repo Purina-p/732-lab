49bbe36912540e486866eb16532f037e8605db1d
Purina-p <127057177+Purina-p@users.noreply.github.com>
2023-03-13 13:42:31 +1300

    exercise-9 work success

--- cs732-lab-01-Purina-p/src/Todos.jsx
+++ cs732-lab-01-Purina-p/src/Todos.jsx
@@ -1,11 +1,11 @@
-function Todos({ todos }) {
+function Todos({ todos, ChangeIsComplete }) {
 
     return (
         <div>
             <ul>
                 {todos.map((todos, index) => (<div key={index}>
-                    <input type="checkbox" checked={todos.isComplete} onchange={}/>{todos.description}
-                    {todos.isComplete&&"(Done!)"}
+                    <input type="checkbox" checked={todos.isComplete} onchange={(e) => ChangeIsComplete(todo, e.target.checked)} />{todos.description}
+                    {todos.isComplete ? <span>(Done!)</span>:<span></span>}
                 </div>))}
             </ul>
         </div>
